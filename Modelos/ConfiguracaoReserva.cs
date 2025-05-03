65851eb9cd030b295f16ff41a1027a8cb0129196
paladini-qa <paladini.qa@gmail.com>
2025-05-03 16:54:05 -0300

    feat: melhorar validações em  ConfiguracaoReserva e Program

--- Modelos/ConfiguracaoReserva.cs
+++ Modelos/ConfiguracaoReserva.cs
@@ -6,25 +6,70 @@ public class ConfiguracaoReserva
   private DateTime _dataMaxima;
   private TimeSpan _horaMinima;
   private TimeSpan _horaMaxima;
+  public String DataMinima
+  {
+    get { return _dataMinima.ToString(); }
+    set { _dataMinima = _validarDataInformada(value); }
+  }
+  public String DataMaxima
+  {
+    get { return _dataMaxima.ToString(); }
+    set
+    {
+      _dataMaxima = _validarDataInformada(value);
+      _validarDataMaximaMenorQueMinima();
+    }
+  }
+  public String HoraMinima
+  {
+    get { return _horaMinima.ToString(); }
+    set
+    {
+      _horaMinima = _validarHoraInformada(value);
+    }
+  }
+  public String HoraMaxima
+  {
+    get { return _horaMaxima.ToString(); }
+    set
+    {
+      _horaMaxima = _validarHoraInformada(value);
+      _validarHoraMaximaMenorQueMinima();
+    }
+  }
+  private DateTime _validarDataInformada(string data)
+  {
+    if (!DateTime.TryParseExact(data,
+                   "dd/MM/yyyy",
+                   System.Globalization.CultureInfo.GetCultureInfo("pt-BR"),
+                   System.Globalization.DateTimeStyles.None,
+                   out DateTime _data))
+    {
+      throw new Exception($"Data {data} Inválida!");
+    }
+    return _data;
+  }
+  private TimeSpan _validarHoraInformada(string hora)
+  {
+    if (!TimeSpan.TryParse(hora, out TimeSpan _hora))
+    {
+      throw new Exception($"Hora {hora} Inválida!");
+    }
+    return _hora;
+  }
 
-  public DateTime DataMinima
+  private void _validarDataMaximaMenorQueMinima()
   {
-    get { return _dataMinima; }
-    set { _dataMinima = value; }
+    if (_dataMaxima < _dataMinima)
+    {
+      throw new Exception($"Data máxima {_dataMaxima:dd/MM/yyyy} menor que data mínima {_dataMinima:dd/MM/yyyy}!");
+    }
   }
-  public DateTime DataMaxima
+  private void _validarHoraMaximaMenorQueMinima()
   {
-    get { return _dataMaxima; }
-    set { _dataMaxima = value; }
-  }
-  public TimeSpan HoraMinima
-  {
-    get { return _horaMinima; }
-    set { _horaMinima = value; }
-  }
-  public TimeSpan HoraMaxima
-  {
-    get { return _horaMaxima; }
-    set { _horaMaxima = value; }
+    if (_horaMaxima < _horaMinima)
+    {
+      throw new Exception($"Hora máxima {HoraMaxima} menor que hora mínima {HoraMinima}!");
+    }
   }
 }
--- Program.cs
+++ Program.cs
@@ -1,55 +1,60 @@
-﻿using Modelos;
+﻿using System.Globalization;
+using Modelos;
 
-Reserva reserva = new();
+ConfiguracaoReserva configuracao = new();
 
-Console.WriteLine("Informe a data mínima (yyyy-MM-dd):");
-DateTime dataMinima = DateTime.Parse(Console.ReadLine());
+Console.WriteLine("Informe a data mínima (dd-MM-yyyy):");
+while (true)
+{
+  try
+  {
+    configuracao.DataMinima = Console.ReadLine() ?? string.Empty;
+    break;
+  }
+  catch (Exception e)
+  {
+    Console.Write($"Erro: {e.Message}\nDigite uma nova data mínima: ");
+  }
+}
 
-Console.WriteLine("Informe a data máxima (yyyy-MM-dd):");
-DateTime dataMaxima = DateTime.Parse(Console.ReadLine());
+Console.WriteLine("Informe a data máxima (dd-MM-yyyy):");
+while (true)
+{
+  try
+  {
+    configuracao.DataMaxima = Console.ReadLine() ?? string.Empty;
+    break;
+  }
+  catch (Exception e)
+  {
+    Console.Write($"Erro: {e.Message}\nDigite uma nova data máxima: ");
+  }
+}
 
 Console.WriteLine("Informe a hora mínima (HH:mm):");
-TimeSpan horaMinima = TimeSpan.Parse(Console.ReadLine());
+while (true)
+{
+  try
+  {
+    configuracao.HoraMinima = Console.ReadLine() ?? string.Empty;
+    break;
+  }
+  catch (Exception e)
+  {
+    Console.Write($"Erro: {e.Message}\nDigite uma nova hora mínima: ");
+  }
+}
 
 Console.WriteLine("Informe a hora máxima (HH:mm):");
-TimeSpan horaMaxima = TimeSpan.Parse(Console.ReadLine());
-
-// Criar a instância da configuração
-ConfiguracaoReserva configuracao = new()
+while (true)
 {
-  DataMinima = dataMinima,
-  DataMaxima = dataMaxima,
-  HoraMinima = horaMinima,
-  HoraMaxima = horaMaxima
-};
-
-Console.WriteLine("Informe a data da reserva (yyyy-MM-dd):");
-DateTime dataReserva = DateTime.Parse(Console.ReadLine());
-
-Console.WriteLine("Informe a hora da reserva (HH:mm):");
-TimeSpan horaReserva = TimeSpan.Parse(Console.ReadLine());
-
-Console.WriteLine("Informe a descrição da sala:");
-string descricaoSala = Console.ReadLine();
-
-Console.WriteLine("Informe a capacidade da sala:");
-int capacidadeSala = int.Parse(Console.ReadLine());
-
-// Criar a instância da reserva, validando as informações conforme a configuração
-if (dataReserva < configuracao.DataMinima || dataReserva > configuracao.DataMaxima)
-{
-  Console.WriteLine("Erro: A data da reserva está fora do intervalo permitido.");
-}
-else if (horaReserva < configuracao.HoraMinima || horaReserva > configuracao.HoraMaxima)
-{
-  Console.WriteLine("Erro: A hora da reserva está fora do intervalo permitido.");
-}
-else
-{
-  reserva.Data = dataReserva;
-  reserva.Hora = horaReserva;
-  reserva.DescricaoSala = descricaoSala;
-  reserva.CapacidadeSala = capacidadeSala;
-
-  Console.WriteLine("Reserva criada com sucesso!");
+  try
+  {
+    configuracao.HoraMaxima = Console.ReadLine() ?? string.Empty;
+    break;
+  }
+  catch (Exception e)
+  {
+    Console.Write($"Erro: {e.Message}\nDigite uma nova hora máxima: ");
+  }
 }
