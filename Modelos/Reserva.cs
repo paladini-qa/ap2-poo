3de4a62ff887c4b3be747bc99882395a5769e489
paladini-qa <paladini.qa@gmail.com>
2025-05-03 15:52:16 -0300

    feat: implementação da ConfiguracaoReserva

--- Modelos/Reserva.cs
+++ Modelos/Reserva.cs
@@ -2,29 +2,4 @@ namespace Modelos;
 
 public class Reserva
 {
-  private DateTime _dataMinima;
-  private DateTime _dataMaxima;
-  private TimeSpan _horaMinima;
-  private TimeSpan _horaMaxima;
-
-  public DateTime DataMinima
-  {
-    get { return _dataMinima; }
-    set { _dataMinima = value; }
-  }
-  public DateTime DataMaxima
-  {
-    get { return _dataMaxima; }
-    set { _dataMaxima = value; }
-  }
-  public TimeSpan HoraMinima
-  {
-    get { return _horaMinima; }
-    set { _horaMinima = value; }
-  }
-  public TimeSpan HoraMaxima
-  {
-    get { return _horaMaxima; }
-    set { _horaMaxima = value; }
-  }
 }
